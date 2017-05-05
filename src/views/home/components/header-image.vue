498a1c0e87ebe8576297043e451fb9fa0bd50069
邱国辉 <530784837@qq.com>
2017-05-05 09:37:55 +0800

    修改名字

--- src/views/home/components/header-image.vue
+++ src/views/home/components/header-image.vue
@@ -8,7 +8,7 @@
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="svg-filters">
 			<defs>
 				<filter id="filter-ripple">
-					<feImage xlink:href="../static/ripple.png" x="30" y="20" width="0" height="0" result="ripple"></feImage>
+					<feImage xlink:href="../static/images/ripple.png" x="30" y="20" width="0" height="0" result="ripple"></feImage>
 					<feDisplacementMap xChannelSelector="R" yChannelSelector="G" color-interpolation-filters="sRGB" in="SourceGraphic" in2="ripple" scale="20" result="dm" />
 					<feComposite operator="in" in2="ripple"></feComposite>
 					<feComposite in2="SourceGraphic"></feComposite>
